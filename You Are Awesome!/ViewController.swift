da3a7365ae86c51710eada621f1005d80bb8de57
Kathryn Tatum <tatumka@bc.edu>
2020-01-29 00:22:02 -0500

    2.14 Play Sounds

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
-    var imageNumber = 0
-    var messageNumber = 0
+    var imageNumber = -1
+    var messageNumber = -1
     let totalNumberOfImages = 9
+    var audioPlayer = AVAudioPlayer()
     
     //var randomMessage = ["You Are Funny!", "You Are Weird!", "You Are Amazing!"]
     
@@ -27,21 +29,30 @@ class ViewController: UIViewController {
         
         let messages = ["You Are Awesome!", "You Are Great!", "You Are Fantastic!", "Fabulous? That's You!", "When the Genuis Bar Needs Help, They Call You!"]
         
-        var newMessageNumber = Int.random(in: 0...messages.count-1)
-        while messageNumber == newMessageNumber {
+        var newMessageNumber: Int
+        repeat {
             newMessageNumber = Int.random(in: 0...messages.count-1)
-        }
+        } while messageNumber == newMessageNumber
         messageNumber = newMessageNumber
         messageLabel.text = messages[messageNumber]
         
-        var newImageNumber = Int.random(in: 0...totalNumberOfImages)
-        while imageNumber == newImageNumber {
+        var newImageNumber: Int
+        repeat {
             newImageNumber = Int.random(in: 0...totalNumberOfImages)
-        }
+        } while imageNumber == newImageNumber
         imageNumber = newImageNumber
         imageView.image = UIImage(named: "image\(imageNumber)")
         
-        
+        if let sound = NSDataAsset(name: "sound0") {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print ("ERROR: 🛑 \(error.localizedDescription) Could not initialize AVAudioPlayer object")
+            }
+        } else {
+            print("ERROR: 🛑 Could not read data from file sound0")
+        }
     }
     
 }
